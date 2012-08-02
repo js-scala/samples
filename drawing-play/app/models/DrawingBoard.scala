aaf3c5d7932c8da996147ddaf46106f2c6e331b0
Nada Amin <namin@alum.mit.edu>
2012-08-03 00:20:50 +0200

    create an actor for each board

--- drawing-play/app/models/DrawingBoard.scala
+++ drawing-play/app/models/DrawingBoard.scala
@@ -16,78 +16,65 @@ import play.api.Play.current
 object DrawingBoard {
   
   implicit val timeout = Timeout(1 second)
-  
-  lazy val default = {
-    val drawingBoardActor = Akka.system.actorOf(Props[DrawingBoardActor])
-    drawingBoardActor
+
+  val colors =  IndexedSeq("red", "blue", "yellow", "green", "purple", "orange")
+  def pickColor(id: Int) = colors(id % colors.length)
+
+  lazy val supervisor = {
+    val drawingBoardSupervisor = Akka.system.actorOf(Props[DrawingBoardSupervisor])
+    drawingBoardSupervisor
   }
 
   def join(name: String, mode: String):Promise[(Iteratee[JsValue,_],Enumerator[JsValue])] = {
-    (default ? Join(name, mode)).asPromise.map {
-      case Connected(id, enumerator) => 
+    (supervisor ? Join(name, mode)).asPromise.map {
+      case Connected(board, id, enumerator) =>
         val iteratee = Iteratee.foreach[JsValue] { event =>
-          default ! ClientAction(name, id, event)
+          board ! ClientAction(id, event)
         }.mapDone { _ =>
-          default ! Quit(name, id)
+          board ! Quit(id)
         }
         (iteratee,enumerator)
     }
   }
 }
 
-class DrawingBoard(val name: String) {
-  var count = 0
-  var members = Map.empty[Int,PushEnumerator[JsValue]]
-  var messages = List.empty[JsValue]
-}
-
-class DrawingBoardActor extends Actor {
-  var boards = Map.empty[String,DrawingBoard]
-  val colors =  IndexedSeq("red", "blue", "yellow", "green", "purple", "orange")
+class DrawingBoard(val name: String) extends Actor {
+  private var count = 0
+  private var members = Map.empty[Int,PushEnumerator[JsValue]]
+  private var messages = List.empty[JsValue]
 
   def receive = {
-    case Join(name, mode) => {
-      val board = boards.get(name) match {
-        case None =>
-          val b = new DrawingBoard(name)
-          boards = boards + (name -> b)
-          b
-        case Some(b) => b
-      }
-      val id = board.count
-      board.count += 1
+    case JoinBoard(client, mode) => {
+      val id = count
+      count += 1
       val channel =  mode match {
         case "goto" =>
           Enumerator.imperative[JsValue]()
         case "replay" =>
-          Enumerator.imperative[JsValue](onStart = self ! Playback(name, id))
+          Enumerator.imperative[JsValue](onStart = self ! Playback(id))
       }
-      board.members = board.members + (id -> channel)
-      sender ! Connected(id, channel)
+      members = members + (id -> channel)
+      client ! Connected(self, id, channel)
     }
-    case Playback(name, id) => {
-      val board = boards(name)
-      board.members.get(id).foreach { channel =>
-        board.messages.reverse.foreach(channel.push)
+    case Playback(id) => {
+      members.get(id).foreach { channel =>
+        messages.reverse.foreach(channel.push)
       }
     }
-    case ClientAction(name, id, js) => {
-      notifyAll(name, id, js)
+    case ClientAction(id, js) => {
+      notifyAll(id, js)
     }
-    case Quit(name, id) => {
-      val board = boards(name)
-      board.members = board.members - id
+    case Quit(id) => {
+      members = members - id
     }
   }
 
-  def pickColor(id: Int) = colors(id % colors.length)
-
-  def notifyAll(name: String, id: Int, js: JsValue) {
+  def notifyAll(id: Int, js: JsValue) {
     val msg = (js \ "action").as[String] match {
       case "move" => Some(JsObject(Seq(
         "action" -> JsString("move"),
         "id"     -> JsString(id.toString),
-        "color"  -> JsString(pickColor(id)),
+        "color"  -> JsString(DrawingBoard.pickColor(id)),
         "cx"     -> js \ "cx",
         "cy"     -> js \ "cy",
         "w"      -> js \ "w",
@@ -95,21 +82,38 @@ class DrawingBoardActor extends Actor {
       )))
       case _ => None
     }
-    val board = boards(name)
     msg match {
       case None => Logger("debug").info("Received unknown message: " + js.toString)
       case Some(msg) => {
-        board.messages = msg :: board.messages
-        board.members.foreach {
+        messages = msg :: messages
+        members.foreach {
           case (_, channel) => channel.push(msg)
         }
       }
     }
-  }  
+  }
+}
+
+class DrawingBoardSupervisor extends Actor {
+  private var boards = Map.empty[String,ActorRef]
+
+  def receive = {
+    case Join(name, mode) => {
+      val board = boards.get(name) match {
+        case None =>
+          val b = context.actorOf(Props(new DrawingBoard(name)))
+          boards = boards + (name -> b)
+          b
+        case Some(b) => b
+      }
+      board ! JoinBoard(sender, mode)
+    }
+  }
 }
 
 case class Join(name: String, mode: String)
-case class Playback(name: String, id: Int)
-case class Quit(name: String, id: Int)
-case class ClientAction(name: String, id: Int, js: JsValue)
-case class Connected(id: Int, enumerator:Enumerator[JsValue])
+case class JoinBoard(client: ActorRef, mode: String)
+case class Playback(id: Int)
+case class Quit(id: Int)
+case class ClientAction(id: Int, js: JsValue)
+case class Connected(board: ActorRef, id: Int, enumerator:Enumerator[JsValue])
